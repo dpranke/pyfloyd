9259496bf33db2fd7792c2f2e0ef1fcbd85d83f3
Dirk Pranke <dpranke@gmail.com>
2025-06-10 16:24:15 -0700

    Merge hard_coded_generator back into python_generator
    Now that I've removed the hard-coded JS generator, there's no
    need for the two classes.

--- src/pyfloyd/python_generator.py
+++ src/pyfloyd/python_generator.py
@@ -16,15 +16,17 @@
 
 from typing import Any, Optional, Union
 
-from pyfloyd import datafile
-from pyfloyd import formatter
-from pyfloyd import generator
-from pyfloyd import grammar as gram
-from pyfloyd import hard_coded_generator
-from pyfloyd import support
+from pyfloyd import (
+    datafile,
+    formatter,
+    generator,
+    grammar as m_grammar,
+    string_literal,
+    support,
+)
 
 
-class PythonGenerator(hard_coded_generator.HardCodedGenerator):
+class PythonGenerator(generator.Generator):
     name: str = 'Python'
     ext: str = '.py'
     indent: Union[int, str] = 4
@@ -55,6 +57,83 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
 
         # Python doesn't need to declare local vars.
         self._process_grammar(local_var_map={})
+
+    def _defmt(self, s: str, dedented=False) -> formatter.VList:
+        if dedented:
+            text = s
+        else:
+            text = datafile.dedent(s)
+        vl = formatter.VList()
+        lines = text.splitlines()
+        for line in lines:
+            vl += line
+        return vl
+
+    def _defmtf(self, s: str, dedented=False, **kwargs) -> formatter.VList:
+        if dedented:
+            text = s
+        else:
+            text = datafile.dedent(s)
+        lines = text.format(**kwargs).splitlines()
+        vl = formatter.VList()
+        for line in lines:
+            vl += line
+        return vl
+
+    def _fmt(self, obj: formatter.FormatObj) -> str:
+        indent = self.indent
+        assert isinstance(indent, str) and indent
+        text = '\n'.join(formatter.flatten(obj, indent=indent)) + '\n'
+        return text
+
+    def _gen_varname(self, name: str) -> str:
+        r = f'v_{name.replace("$", "_")}'
+        return r
+
+    def _gen_lit(self, lit: str) -> str:
+        return string_literal.encode(lit)
+
+    def _gen_expr(self, node: m_grammar.Node) -> formatter.FormatObj:
+        fn = getattr(self, f'_ty_{node.t}')
+        return fn(node)
+
+    def _gen_stmts(self, node: m_grammar.Node) -> formatter.VList:
+        fn = getattr(self, f'_ty_{node.t}')
+        r = fn(node)
+        if not isinstance(r, formatter.VList):
+            r = formatter.VList(r)
+        return r
+
+    def _gen_needed_methods(self) -> formatter.FormatObj:
+        obj = formatter.VList()
+
+        def add_method(name: str):
+            nonlocal obj
+            obj += ''
+            obj += self._defmt(self._builtin_methods[name], dedented=True)
+
+        if self.grammar.ch_needed:
+            add_method('ch')
+        add_method('error')
+        add_method('fail')
+        if self.grammar.leftrec_needed:
+            add_method('leftrec')
+        if self.grammar.lookup_needed:
+            add_method('lookup')
+        if self.options.memoize:
+            add_method('memoize')
+        add_method('offsets')
+        if self.grammar.operator_needed:
+            add_method('operator')
+        if self.grammar.range_needed:
+            add_method('range')
+        add_method('rewind')
+        if self.grammar.str_needed:
+            add_method('str')
+        add_method('succeed')
+        if self.grammar.unicat_needed:
+            add_method('unicat')
+        return obj
 
     def generate(self) -> str:
         vl = formatter.VList()
@@ -420,7 +499,7 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         return obj
 
     def _gen_method_text(
-        self, method_name: str, node: gram.Node
+        self, method_name: str, node: m_grammar.Node
     ) -> formatter.VList:
         return formatter.VList(
             formatter.HList('def _', method_name, '(self):'),
@@ -447,11 +526,42 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
             'self._' + fn, formatter.Triangle('(', formatter.Comma(*args), ')')
         )
 
+    def _gen_main_footer(self) -> formatter.VList:
+        return self._defmt("""
+
+
+            if __name__ == '__main__':
+                sys.exit(main())
+            """)
+
+    def _gen_default_footer(self) -> formatter.VList:
+        return formatter.VList()
+
     #
     # Handlers for each non-host node in the glop AST follow.
     #
 
-    def _ty_choice(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_action(self, node: m_grammar.Node) -> formatter.FormatObj:
+        return formatter.HList(
+            self._gen_invoke(
+                self._gen_opname('succeed'), self._gen_expr(node.child)
+            ),
+            self._map['end'],
+        )
+
+    def _ty_apply(self, node: m_grammar.Node) -> formatter.FormatObj:
+        if node.memoize:
+            return formatter.HList(
+                self._gen_invoke(
+                    self._gen_opname('memoize'),
+                    f"'{node.v}'",
+                    self._gen_rulename(node.v),
+                ),
+                self._map['end'],
+            )
+        return formatter.HList(self._gen_invoke(node.v), self._map['end'])
+
+    def _ty_choice(self, node: m_grammar.Node) -> formatter.FormatObj:
         vl = formatter.VList('p = self._pos')
         for subnode in node.ch[:-1]:
             vl += self._gen_stmts(subnode)
@@ -461,7 +571,7 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         vl += self._gen_stmts(node.ch[-1])
         return vl
 
-    def _ty_count(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_count(self, node: m_grammar.Node) -> formatter.FormatObj:
         vl = formatter.VList(
             'vs = []',
             'i = 0',
@@ -481,7 +591,98 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         vl += 'self._o_succeed(vs)'
         return vl
 
-    def _ty_ends_in(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_e_arr(
+        self, node: m_grammar.Node
+    ) -> Union[str, formatter.Triangle]:
+        if len(node.ch) == 0:
+            return '[]'
+        args = [self._gen_expr(c) for c in node.ch]
+        return formatter.Triangle('[', formatter.Comma(*args), ']')
+
+    def _ty_e_call(self, node: m_grammar.Node) -> formatter.Triangle:
+        # e_{call,getitem,qual} have been rewritten to e_{call,getitem}_infix.
+        del node
+        assert False, '`e_call` should never be invoked'
+
+    def _ty_e_call_infix(self, node: m_grammar.Node) -> formatter.Pack:
+        pfx = self._gen_expr(node.ch[0])
+        args = [self._gen_expr(c) for c in node.ch[1:]]
+        if node.ch[0].v in self.grammar.externs:
+            return formatter.Pack(
+                pfx,
+                formatter.Triangle('(', formatter.Comma('self', *args), ')'),
+            )
+        return formatter.Pack(
+            pfx, formatter.Triangle('(', formatter.Comma(*args), ')')
+        )
+
+    def _ty_e_const(self, node: m_grammar.Node) -> str:
+        assert isinstance(node.v, str)
+        return self._map[node.v]
+
+    def _ty_e_getitem(self, node: m_grammar.Node) -> formatter.Triangle:
+        # e_{call,getitem,qual} have been rewritten to e_{call,getitem}_infix.
+        del node
+        assert False, '`e_getitem` should never be invoked'
+
+    def _ty_e_getitem_infix(self, node: m_grammar.Node) -> formatter.HList:
+        return formatter.HList(
+            self._gen_expr(node.ch[0]),
+            formatter.Triangle('[', self._gen_expr(node.ch[1]), ']'),
+        )
+
+    def _ty_e_lit(self, node: m_grammar.Node) -> str:
+        return self._gen_lit(node.v)
+
+    def _ty_e_minus(self, node: m_grammar.Node) -> formatter.Tree:
+        return formatter.Tree(
+            self._gen_expr(node.ch[0]), '-', self._gen_expr(node.ch[1])
+        )
+
+    def _ty_e_not(self, node: m_grammar.Node) -> formatter.Tree:
+        return formatter.Tree(
+            None, self._map['not'], self._gen_expr(node.child)
+        )
+
+    def _ty_e_num(self, node: m_grammar.Node) -> str:
+        assert isinstance(node.v, str)
+        return str(node.v)
+
+    def _ty_e_paren(self, node: m_grammar.Node) -> formatter.FormatObj:
+        return self._gen_expr(node.child)
+
+    def _ty_e_plus(self, node: m_grammar.Node) -> formatter.Tree:
+        return formatter.Tree(
+            self._gen_expr(node.ch[0]), '+', self._gen_expr(node.ch[1])
+        )
+
+    def _ty_e_qual(self, node: m_grammar.Node) -> formatter.Pack:
+        # e_{call,getitem,qual} have been rewritten to e_{call,getitem}_infix.
+        del node
+        assert False, '`e_qual` should never be invoked'
+
+    def _ty_e_ident(self, node: m_grammar.Node) -> formatter.El:
+        if node.attrs.kind == 'outer':
+            return self._gen_invoke(
+                self._gen_opname('lookup'), "'" + node.v + "'"
+            )
+        if node.attrs.kind == 'extern':
+            return self._gen_extern(node.v)
+        if node.attrs.kind == 'function':
+            return self._gen_funcname(node.v)
+        assert node.attrs.kind == 'local', (
+            f'Unexpected identifer kind {node!r}'
+        )
+        return self._gen_varname(node.v)
+
+    def _ty_empty(self, node) -> formatter.FormatObj:
+        del node
+        return formatter.HList(
+            self._gen_invoke(self._gen_opname('succeed'), self._map['null']),
+            self._map['end'],
+        )
+
+    def _ty_ends_in(self, node: m_grammar.Node) -> formatter.FormatObj:
         vl = formatter.VList('while True:')
         vl += formatter.Indent(self._gen_stmts(node.child))
         if node.can_fail:
@@ -493,7 +694,13 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         ]
         return vl
 
-    def _ty_label(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_equals(self, node) -> formatter.FormatObj:
+        arg = self._gen_expr(node.child)
+        return formatter.HList(
+            self._gen_invoke(self._gen_opname('str'), arg), self._map['end']
+        )
+
+    def _ty_label(self, node: m_grammar.Node) -> formatter.FormatObj:
         vl = formatter.VList(self._gen_stmts(node.child))
         if node.child.can_fail:
             vl += ['if self._failed:', '    return']
@@ -503,7 +710,33 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
             vl += f'{self._gen_varname(node.v)} = self._val'
         return vl
 
-    def _ty_not(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_leftrec(self, node) -> formatter.FormatObj:
+        if node.attrs.left_assoc:
+            left_assoc = self._map['true']
+        else:
+            left_assoc = self._map['false']
+
+        return formatter.HList(
+            self._gen_invoke(
+                self._gen_opname('leftrec'),
+                self._gen_rulename(node.child.v),
+                "'" + node.v + "'",
+                left_assoc,
+            ),
+            self._map['end'],
+        )
+
+    def _ty_lit(self, node) -> formatter.FormatObj:
+        expr = self._gen_lit(node.v)
+        if len(node.v) == 1:
+            method = 'o_ch'
+        else:
+            method = 'o_str'
+        return formatter.HList(
+            self._gen_invoke(method, expr), self._map['end']
+        )
+
+    def _ty_not(self, node: m_grammar.Node) -> formatter.FormatObj:
         vl = formatter.VList(
             'p = self._pos',
             'errpos = self._errpos',
@@ -519,14 +752,24 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         ]
         return vl
 
-    def _ty_not_one(self, node: gram.Node) -> formatter.FormatObj:
-        sublines = self._gen_stmts(gram.Node('not', None, [node.child]))
+    def _ty_not_one(self, node: m_grammar.Node) -> formatter.FormatObj:
+        sublines = self._gen_stmts(m_grammar.Node('not', None, [node.child]))
         vl = formatter.VList()
         vl += sublines
         vl += ['if not self._failed:', '    self._r_any()']
         return vl
 
-    def _ty_opt(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_operator(self, node) -> formatter.FormatObj:
+        # Operator nodes have no children, but subrules for each arm
+        # of the expression cluster have been defined and are referenced
+        # from self.grammar.operators[node.v].choices.
+        assert node.ch == []
+        return formatter.HList(
+            self._gen_invoke(self._gen_opname('operator'), "'" + node.v + "'"),
+            self._map['end'],
+        )
+
+    def _ty_opt(self, node: m_grammar.Node) -> formatter.FormatObj:
         vl = formatter.VList('p = self._pos')
         vl += self._gen_stmts(node.child)
         vl += [
@@ -537,10 +780,10 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         ]
         return vl
 
-    def _ty_paren(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_paren(self, node: m_grammar.Node) -> formatter.FormatObj:
         return self._gen_stmts(node.child)
 
-    def _ty_plus(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_plus(self, node: m_grammar.Node) -> formatter.FormatObj:
         sublines = self._gen_stmts(node.child)
         vl = formatter.VList('vs = []')
         vl += sublines
@@ -559,7 +802,7 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         ]
         return vl
 
-    def _ty_pred(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_pred(self, node: m_grammar.Node) -> formatter.FormatObj:
         arg = self._gen_expr(node.child)
         return formatter.VList(
             formatter.HList('v = ', arg),
@@ -571,7 +814,7 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
             "    raise _ParsingRuntimeError('Bad predicate value')",
         )
 
-    def _ty_regexp(self, node: gram.Node) -> formatter.FormatObj:
+    def _ty_regexp(self, node: m_grammar.Node) -> formatter.FormatObj:
         return formatter.VList(
             f'p = {self._gen_lit(node.v)}',
             'if p not in self._regexps:',
@@ -583,7 +826,7 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
             'self._o_fail()',
         )
 
-    def _ty_run(self, node: gram.Node) -> formatter.VList:
+    def _ty_run(self, node: m_grammar.Node) -> formatter.VList:
         vl = formatter.VList('start = self._pos')
         vl += self._gen_stmts(node.child)
         if node.child.can_fail:
@@ -594,13 +837,23 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         ]
         return vl
 
-    def _ty_scope(self, node: gram.Node) -> formatter.VList:
+    def _ty_scope(self, node: m_grammar.Node) -> formatter.VList:
         vl = formatter.VList('self._scopes.append({})')
         vl += self._gen_stmts(node.child)
         vl += 'self._scopes.pop()'
         return vl
 
-    def _ty_seq(self, node: gram.Node) -> formatter.VList:
+    def _ty_range(self, node) -> formatter.FormatObj:
+        return formatter.HList(
+            self._gen_invoke(
+                self._gen_opname('range'),
+                self._gen_lit(node.v[0]),
+                self._gen_lit(node.v[1]),
+            ),
+            self._map['end'],
+        )
+
+    def _ty_seq(self, node: m_grammar.Node) -> formatter.VList:
         vl = formatter.VList(self._gen_stmts(node.ch[0]))
         if node.ch[0].can_fail:
             vl += ['if self._failed:', '    return']
@@ -611,7 +864,11 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         vl += self._gen_stmts(node.ch[-1])
         return vl
 
-    def _ty_star(self, node: gram.Node) -> formatter.VList:
+    def _ty_set(self, node) -> formatter.FormatObj:
+        new_node = m_grammar.Node('regexp', '[' + node.v + ']', [])
+        return self._ty_regexp(new_node)
+
+    def _ty_star(self, node: m_grammar.Node) -> formatter.VList:
         sublines = self._gen_stmts(node.child)
         vl = formatter.VList(
             'vs = []',
@@ -628,16 +885,13 @@ class PythonGenerator(hard_coded_generator.HardCodedGenerator):
         ]
         return vl
 
-    def _gen_main_footer(self) -> formatter.VList:
-        return self._defmt("""
-
-
-            if __name__ == '__main__':
-                sys.exit(main())
-            """)
-
-    def _gen_default_footer(self) -> formatter.VList:
-        return formatter.VList()
+    def _ty_unicat(self, node) -> formatter.FormatObj:
+        return formatter.HList(
+            self._gen_invoke(
+                self._gen_opname('unicat'), self._gen_lit(node.v)
+            ),
+            self._map['end'],
+        )
 
 
 _BUILTINS = {
