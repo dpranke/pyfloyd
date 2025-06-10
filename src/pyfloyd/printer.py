1ee2c7a765a3f4b835e51f4c1318a39bc999145a
Dirk Pranke <dpranke@gmail.com>
2025-06-10 15:31:32 -0700

    Somehow missed updating printer.py w/ the Node cleanup.

--- src/pyfloyd/printer.py
+++ src/pyfloyd/printer.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pyfloyd import grammar
 from pyfloyd import string_literal
 
 
@@ -27,16 +28,18 @@ class Printer:
 
     def _build_rules(self):
         rules = []
-        for _, rule_name, node in self._ast[2]:
+        for c in self._ast.ch:
+            rule_name = c.v
+            node = c.child
             self._max_rule_len = max(len(rule_name), self._max_rule_len)
-            cs = self._fmt_rule(node[0])
+            cs = self._fmt_rule(node)
             rules.append((rule_name, cs))
         return rules
 
     def _fmt_rule(self, node):
         cs = []
-        if node[0] == 'choice':
-            for choice_node in node[2]:
+        if node.t == 'choice':
+            for choice_node in node.ch:
                 choice, action = self._split_action(choice_node)
                 self._max_choice_len = max(len(choice), self._max_choice_len)
                 cs.append((choice, action))
@@ -47,13 +50,13 @@ class Printer:
         return cs
 
     def _split_action(self, node):
-        if node[0] == 'scope':
-            return self._split_action(node[2][0])
-        if node[0] != 'seq' or node[2][-1][0] != 'action':
+        if node.t == 'scope':
+            return self._split_action(node.child)
+        if node.t != 'seq' or node.ch[-1].t != 'action':
             return (self._proc(node), '')
         return (
-            self._proc(['seq', None, node[2][:-1]]),
-            self._proc(node[2][-1]),
+            self._proc(grammar.Node('seq', None, node.ch[:-1])),
+            self._proc(node.ch[-1]),
         )
 
     def _format_rules(self, rules):
@@ -68,7 +71,7 @@ class Printer:
         return '\n'.join(lines).strip() + '\n'
 
     def _proc(self, node):
-        fn = getattr(self, f'_ty_{node[0]}')
+        fn = getattr(self, f'_ty_{node.t}')
         return fn(node)
 
     #
@@ -76,114 +79,113 @@ class Printer:
     #
 
     def _ty_action(self, node):
-        return '-> ' + self._proc(node[2][0])
+        return '-> ' + self._proc(node.child)
 
     def _ty_apply(self, node):
-        return node[1]
+        return node.v
 
     def _ty_choice(self, node):
-        return ' | '.join(self._proc(e) for e in node[2])
+        return ' | '.join(self._proc(e) for e in node.ch)
 
     def _ty_count(self, node):
-        if node[1][0] == node[1][1]:
-            return '%s{%d}' % (self._proc(node[2][0]), node[1][0])
-        return '%s{%d,%d}' % (self._proc(node[2][0]), node[1][0], node[1][1])
+        if node.v[0] == node.v[1]:
+            return '%s{%d}' % (self._proc(node.child), node.v[0])
+        return '%s{%d,%d}' % (self._proc(node.child), node.v[0], node.v[1])
 
     def _ty_e_arr(self, node):
-        return '[' + ', '.join(self._proc(el) for el in node[2]) + ']'
+        return '[' + ', '.join(self._proc(el) for el in node.ch) + ']'
 
     def _ty_e_call(self, node):
-        return '(' + ', '.join(self._proc(arg) for arg in node[2]) + ')'
+        return '(' + ', '.join(self._proc(arg) for arg in node.ch) + ')'
 
     def _ty_e_const(self, node):
-        return node[1]
+        return node.v
 
     def _ty_e_getitem(self, node):
-        return '[' + self._proc(node[2][0]) + ']'
+        return '[' + self._proc(node.child) + ']'
 
     def _ty_e_lit(self, node):
         return self._ty_lit(node)
 
     def _ty_e_minus(self, node):
-        return self._proc(node[2][0]) + ' - ' + self._proc(node[2][1])
+        return self._proc(node.ch[0]) + ' - ' + self._proc(node.ch[1])
 
     def _ty_e_not(self, node):
-        return '!' + self._proc(node[2][0])
+        return '!' + self._proc(node.child)
 
     def _ty_e_num(self, node):
-        return str(node[1])
+        return str(node.v)
 
     def _ty_e_plus(self, node):
-        return self._proc(node[2][0]) + ' + ' + self._proc(node[2][1])
+        return self._proc(node.ch[0]) + ' + ' + self._proc(node.ch[1])
 
     def _ty_e_qual(self, node):
-        _, _, ops = node
-        v = self._proc(ops[0])
-        return v + ''.join(self._proc(op) for op in ops[1:])
+        v = self._proc(node.ch[0])
+        return v + ''.join(self._proc(op) for op in node.ch[1:])
 
     def _ty_e_ident(self, node):
-        return node[1]
+        return node.v
 
     def _ty_empty(self, node):
         del node
         return ''
 
     def _ty_ends_in(self, node):
-        return '^.' + self._proc(node[2][0])
+        return '^.' + self._proc(node.child)
 
     def _ty_label(self, node):
-        if node[1].startswith('$'):
-            return self._proc(node[2][0])
-        return self._proc(node[2][0]) + ':' + node[1]
+        if node.v.startswith('$'):
+            return self._proc(node.child)
+        return self._proc(node.child) + ':' + node.v
 
     def _ty_leftrec(self, node):
-        return self._proc(node[2][0])
+        return self._proc(node.child)
 
     def _ty_lit(self, node):
-        return string_literal.encode(node[1])
+        return string_literal.encode(node.v)
 
     def _ty_not(self, node):
-        return '~' + self._proc(node[2][0])
+        return '~' + self._proc(node.child)
 
     def _ty_not_one(self, node):
-        return '^' + self._proc(node[2][0])
+        return '^' + self._proc(node.child)
 
     def _ty_opt(self, node):
-        return self._proc(node[2][0]) + '?'
+        return self._proc(node.child) + '?'
 
     def _ty_paren(self, node):
-        return '(' + self._proc(node[2][0]) + ')'
+        return '(' + self._proc(node.child) + ')'
 
     def _ty_plus(self, node):
-        return self._proc(node[2][0]) + '+'
+        return self._proc(node.child) + '+'
 
     def _ty_pred(self, node):
-        return '?{ ' + self._proc(node[2][0]) + ' }'
+        return '?{ ' + self._proc(node.child) + ' }'
 
     def _ty_range(self, node):
         return (
-            string_literal.encode(node[1][0])
+            string_literal.encode(node.v[0])
             + '..'
-            + string_literal.encode(node[1][1])
+            + string_literal.encode(node.v[1])
         )
 
     def _ty_regexp(self, node):
-        return '/' + string_literal.escape(node[1], '/') + '/'
+        return '/' + string_literal.escape(node.v, '/') + '/'
 
     def _ty_run(self, node):
-        return '<' + self._proc(node[2][0]) + '>'
+        return '<' + self._proc(node.child) + '>'
 
     def _ty_scope(self, node):
-        return self._proc(node[2][0])
+        return self._proc(node.child)
 
     def _ty_seq(self, node):
-        return ' '.join(self._proc(e) for e in node[2])
+        return ' '.join(self._proc(e) for e in node.ch)
 
     def _ty_set(self, node):
-        return '[' + string_literal.escape(node[1], ']') + ']'
+        return '[' + string_literal.escape(node.v, ']') + ']'
 
     def _ty_star(self, node):
-        return self._proc(node[2][0]) + '*'
+        return self._proc(node.child) + '*'
 
     def _ty_unicat(self, node):
-        return '\\p{' + node[1] + '}'
+        return '\\p{' + node.v + '}'
